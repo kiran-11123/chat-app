8af3adcfd53140b7cc909ca86e9d21a72c1c7b97
Kiran <kiran.sadara6730@gmail.com>
2025-10-05 13:13:20 +0530

    chat application is done

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -2,7 +2,20 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8081 });
+let userCount = 0;
+let allSockets = [];
 wss.on('connection', (socket) => {
+    allSockets.push(socket);
     console.log("websocket server connected..");
-    socket.send("hi there");
+    userCount = userCount + 1;
+    console.log("user connected #" + userCount);
+    socket.on("message", (message) => {
+        console.log("message received " + message.toString());
+        for (let i = 0; i < allSockets.length; i++) {
+            const s = allSockets[i];
+            if (s !== socket) {
+                s.send(message.toString() + " Sent by Server " + i);
+            }
+        }
+    });
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,10 +4,30 @@ const wss = new WebSocketServer({port:8081});
 
 let userCount = 0;
 
+let allSockets =[]
 
 wss.on('connection' , (socket)=>{
+
+    allSockets.push(socket);
      
      console.log("websocket server connected..")
+     userCount=userCount+1;
 
-     socket.send("hi there")
+    
+     console.log("user connected #" + userCount);
+
+
+     socket.on("message" , (message)=>{
+          console.log("message received " + message.toString() );
+
+          for(let i=0;i<allSockets.length ;i++){
+                
+            const s = allSockets[i];
+
+            if(s!==socket){
+                 s.send(message.toString() + " Sent by Server " + i);
+            }
+          }
+          
+     })
 } )
